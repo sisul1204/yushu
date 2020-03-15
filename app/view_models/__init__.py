c63d8132b92f56006f42191f2fd16c191e8c69da
sisul1204 <lizhipeng1021@163.com>
2020-03-15 20:56:55 +0800

    增加viewmodel

---
+++ app/view_models/__init__.py
@@ -0,0 +1,3 @@
+# * coding:utf-8 *
+# Author:sisul
+#创建时间：2020/3/15 17:44
--- app/web/book.py
+++ app/web/book.py
@@ -8,7 +8,7 @@ from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
 from . import web
 from app.forms.book import SearchForm
-
+from ..view_models.book import BookViewModel
 
 
 @web.route('/book/test')
@@ -41,8 +41,10 @@ def search():
         isbn_or_key = is_isbn_or_key(q)
         if isbn_or_key == 'isbn':
             result = YuShuBook.search_by_isbn(q)
+            result = BookViewModel.package_single(result, q)
         else:
             result = YuShuBook.search_by_keyword(q, page)
+            result = BookViewModel.package_collection(result, q)
         return jsonify(result)
     else:
         return jsonify(form.errors)
